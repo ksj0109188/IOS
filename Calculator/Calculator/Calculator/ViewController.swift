ebecad3b48556215c2782f8ed5894a8d4d7613d8
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-02-03 22:50:44 +0900

    Update ViewController.swift
    계산기능 구현

--- Calculator/Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/Calculator/ViewController.swift
@@ -38,22 +38,77 @@ class ViewController: UIViewController {
             self.displayNumber += numberValue
             self.numberOutputLabel.text = displayNumber
         }
-        
     }
     
     @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOperation = .unknown
+        self.numberOutputLabel.text = "0"
     }
+    
     @IBAction func tapDotButton(_ sender: UIButton) {
+        //if and 조건(if 조건문1, 조건문2)
+        if self.displayNumber.count < 8, !self.displayNumber.contains("."){
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.numberOutputLabel.text = displayNumber
+        }
     }
     @IBAction func tapDivideButton(_ sender: UIButton) {
+        self.operation(.divide)
     }
+    
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        self.operation(.multiply)
     }
+    
     @IBAction func tapSubtractButton(_ sender: UIButton) {
+        self.operation(.subtract)
     }
+    
     @IBAction func tapPlusButton(_ sender: UIButton) {
+        self.operation(.Add)
     }
+    
     @IBAction func tapEqualButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation){
+        if self.currentOperation != .unknown{
+            if !self.displayNumber.isEmpty{
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                guard let firstOperand = Double(self.firstOperand) else {return}
+                guard let secondOperand = Double(self.secondOperand) else {return}
+                switch self.currentOperation{
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                case .divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                default:
+                    break
+                }
+                //truncatingRemainder(dividingBy : 나눌수) -> return 나머지
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0{
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            self.currentOperation = operation
+        }  else {
+            self.firstOperand = displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
     }
 }
 
