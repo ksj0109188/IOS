53475221ea9b1e2998f981a8afd85faf6056e065
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-02-10 22:31:04 +0900

    Detail Diary UI 추가

---
+++ Diary/Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 김성준 on 2022/02/10.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
