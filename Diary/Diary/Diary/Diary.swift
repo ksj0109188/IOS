66e2c41b68e484ad6ce923d668f6bd8000e9776b
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-02-13 12:29:43 +0900

    CollectionView에 일기작성 내용 표시하기
    Diary 구조체 추가

---
+++ Diary/Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 김성준 on 2022/02/12.
+//
+
+import Foundation
+
+struct Diary{
+    var title : String
+    var contents : String
+    var date : Date
+    var isStar: Bool
+}
