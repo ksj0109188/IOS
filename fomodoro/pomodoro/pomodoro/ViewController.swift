94219e67cba0c755693828087887da39a8a0f7f0
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-03 22:05:59 +0900

    tomato 이미지 회전
    CGAffineTransform을 활용한 이미지 회전

--- fomodoro/pomodoro/pomodoro/ViewController.swift
+++ fomodoro/pomodoro/pomodoro/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var toggleButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     
     var duration = 60
     var timerStatus:TimereStatus = .end
@@ -52,6 +53,14 @@ class ViewController: UIViewController {
                 let seconds = (self.currentSeconds % 3600) % 60
                 self.timerLabel.text = String(format: "%02d:%02d:%02d", hour,seconds,minutes)
                 self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                //이미지 180도 회전
+                UIView.animate(withDuration: 0.5, delay: 0,animations: {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                })
+                //이미지 360도 회전
+                UIView.animate(withDuration: 0.5, delay: 0.5,animations: {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi*2)
+                })
                 if self.currentSeconds <= 0{
                     self.stopTimer()
                     AudioServicesPlaySystemSound(1005)
@@ -71,6 +80,7 @@ class ViewController: UIViewController {
             self.timerLabel.alpha = 0
             self.progressView.alpha = 0
             self.datePicker.alpha = 1
+            self.imageView.transform = .identity
         })
         self.toggleButton.isSelected = false
         self.timer?.cancel()
