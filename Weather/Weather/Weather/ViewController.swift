bb05d8307597bb1cf2e2c06c6f2744ef2e1c4a99
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-05 21:18:24 +0900

    api통신후 화면에 데이터 표시하기

--- Weather/Weather/Weather/ViewController.swift
+++ Weather/Weather/Weather/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,13 +33,26 @@ class ViewController: UIViewController {
     func getCurrentWeather(cityName:String){
         guard let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=22f8587848fe0871812e2e7f56b38bb8") else {return}
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url){
+        session.dataTask(with: url){ [weak self]
             data, response, error in
             guard let data = data, error == nil else {return}
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from:data)
-            debugPrint(weatherInformation)
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from:data) else {return}
+            DispatchQueue.main.async {
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
         }.resume()
+    }
+    
+    func configureView(weatherInformation:WeatherInformation){
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first{
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C"
+        self.minTempLabel.text = "최저 :\(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "최저 :\(Int(weatherInformation.temp.maxTemp - 273.15))°C"
         
     }
 
