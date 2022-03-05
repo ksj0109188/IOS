ec7fd0ffd6e7a6460bd40961c49ce69b0a0ba646
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-05 11:16:03 +0900

    UI outlet변수 추가

--- Weather/Weather/Weather/ViewController.swift
+++ Weather/Weather/Weather/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    
+    @IBAction func tapFetchWeatherButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
