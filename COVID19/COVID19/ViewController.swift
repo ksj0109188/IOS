bf75b82068b98344e7f9edb273b0f3c5561ce248
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-06 21:45:07 +0900

    프로젝트 생성

---
+++ COVID19/COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 김성준 on 2022/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
