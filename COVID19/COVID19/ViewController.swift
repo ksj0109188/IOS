4e8dd51a32ee59b9ce25028ba972869eaffef1f2
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-07 23:09:22 +0900

    Alamofire를 통한 API통신

--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -7,16 +7,47 @@
 
 import UIKit
 import Charts
+import Alamofire
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    
     @IBOutlet weak var totalCaseLabel: UILabel!
     @IBOutlet weak var newCaseLabel: UILabel!
     @IBOutlet weak var pieChartView: PieChartView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+            
+        })
+    }
+
+    func fetchCovidOverview(completionHandler : @escaping (Result<CityCovidOverview, Error>) -> Void){
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey":"wpYkP4FhdVTGQq9JbgE12czM8ZuCIrDXt"
+        ]
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result{
+                case let .success(data):
+                    do{
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    }catch{
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+        })
+    }
 }
 
