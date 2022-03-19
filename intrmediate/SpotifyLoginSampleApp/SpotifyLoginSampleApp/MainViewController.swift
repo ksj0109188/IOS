7ff4f501b2ff48504da0c35dec2c284d37c3647d
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-19 20:48:31 +0900

    이메일 비밀번호 번경
    등록된 이메일 비밀번호 변경
    로근에 사용된 이메일로 Firebase가 password reset email을 보내게됨

--- intrmediate/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ intrmediate/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import FirebaseAuth
 class MainViewController: UIViewController{
     @IBOutlet weak var welcomLabel: UILabel!
+    @IBOutlet weak var resetPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +27,18 @@ class MainViewController: UIViewController{
                             환영합니다.
                             \(email)님
                            """
+        
+        let isEmailSignIn = Auth.auth().currentUser?.providerData[0].providerID == "password"
+        resetPasswordButton.isHidden = !isEmailSignIn
+        
+        
+    }
+    
+    @IBAction func restPasswordButtonTapeed(_ sender: UIButton) {
+        //사용자의 이메일로 비밀번호 리셋 이메일을 전송.
+        let email = Auth.auth().currentUser?.email ?? ""
+        Auth.auth().sendPasswordReset(withEmail: email,completion: nil)
+        
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
