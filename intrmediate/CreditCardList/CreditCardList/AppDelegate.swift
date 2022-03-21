214655136a03ae8a56e29817aa26d4760bae6356
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-21 21:20:21 +0900

    pod 재설정
    1. FireStore API가 존재하지 않아 pod 재설정 후 재설치

--- intrmediate/CreditCardList/CreditCardList/AppDelegate.swift
+++ intrmediate/CreditCardList/CreditCardList/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestoreSwift
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+
+        let db = Firestore.firestore()
+        
         return true
     }
 
