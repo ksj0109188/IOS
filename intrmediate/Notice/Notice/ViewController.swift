f455ed41cf23f62938c3da5b962ccebd0735323c
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-22 21:29:45 +0900

    프로젝트 생성

---
+++ intrmediate/Notice/Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notice
+//
+//  Created by 김성준 on 2022/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
