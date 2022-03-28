184bd0a7796dd2169a82c4c45de0abf90862c92e
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-28 23:40:36 +0900

    프로젝트 생성
    1. 재난문자 푸시 알림 프로젝트

---
+++ intrmediate/warnning/warnning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  warnning
+//
+//  Created by 김성준 on 2022/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
