f96ef2f1299215f30ac98a82f854815918c9e1bf
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-03-31 21:15:22 +0900

    Source로 view 생성하기
    1. UICollectionView
    2. SceneDelegate에서 Controller 생성하기(Root navigationController -> Collection View를 가지는 )

---
+++ intrmediate/NetflixStyleSampleApp/NetflixStyleSampleApp/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  NetflixStyleSampleApp
+//
+//  Created by 김성준 on 2022/03/31.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController{
+    
+}
