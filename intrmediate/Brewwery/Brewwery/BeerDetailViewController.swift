4114ee2948c9eab73f46fe345f0c28bc79b1a14f
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-04-12 21:39:45 +0900

    맥주 상세화면 UI

--- intrmediate/Brewwery/Brewwery/BeerDetailViewController.swift
+++ intrmediate/Brewwery/Brewwery/BeerDetailViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class BeerDetaukVuewController : UITableViewController{
+class BeerDetailViewController : UITableViewController{
     var beer: Beer?
     
     override func viewDidLoad() {
@@ -34,6 +34,56 @@ class BeerDetaukVuewController : UITableViewController{
 }
 //UITableView DataSource, Delegate
 
-//extension BeerDetaukVuewController{
-//
-//}
+extension BeerDetailViewController{
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case 3:
+            return beer?.foodParing?.count ?? 0
+        default:
+            return 1
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section{
+        case 0:
+            return "ID"
+        case 1:
+            return "Description"
+        case 2:
+            return "Brewers Tips"
+        case 3:
+            let isFoodParingEmpty = beer?.foodParing?.isEmpty ?? true
+            return isFoodParingEmpty ? nil : "Food Paring"
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "BeerDetailListCell")
+        
+        cell.textLabel?.numberOfLines=0
+        cell.selectionStyle = .none
+        switch indexPath.section{
+        case 0:
+            cell.textLabel?.text = String(describing: beer?.id ?? 0)
+            return cell
+        case 1:
+            cell.textLabel?.text = beer?.description ?? "설명 없는 맥주"
+            return cell
+        case 2:
+            cell.textLabel?.text = beer?.brewersTips ?? "팁 없는 맥주"
+            return cell
+        case 3:
+            cell.textLabel?.text = beer?.foodParing?[indexPath.row]
+            return cell
+        default :
+            return cell
+        }
+    }
+}
