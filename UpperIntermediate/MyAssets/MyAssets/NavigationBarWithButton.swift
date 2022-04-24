465f6691f4959f831cc4a67eece1ba776c26f772
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-04-24 15:13:19 +0900

    SwiftUI로 navigation bar 만들기

---
+++ UpperIntermediate/MyAssets/MyAssets/NavigationBarWithButton.swift
@@ -0,0 +1,55 @@
+//
+//  NavigationBarWithButton.swift
+//  MyAssets
+//
+//  Created by 김성준 on 2022/04/24.
+//
+
+import SwiftUI
+
+struct NavigationBarWithButton: ViewModifier {
+    var title:String = ""
+    func body(content: Content) -> some View {
+        return content.navigationBarItems(
+            leading: Text(title)
+                .font(.system(size: 24 ,weight: .bold))
+                .padding(),
+            trailing: Button(
+                action: {
+                    print("자산추가 버튼 Tapped")
+                },
+                label: {
+                    Image(systemName: "plus")
+                    Text("자산추가").font(.system(size: 12))
+                }
+            )
+            .accentColor(.black)
+            .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+        )
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear{
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundColor = UIColor(white: 1, alpha: 0.5)
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+}
+
+extension View{
+    func navigationBarWithButtonStyle(_ title:String) -> some View{
+        return self.modifier(NavigationBarWithButton(title: title))
+        
+    }
+}
+
+struct NavigationBarWithButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            Color.gray.edgesIgnoringSafeArea(.all).navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
