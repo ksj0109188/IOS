391799e66bd475840994db95964313374983868b
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-05-10 22:21:38 +0900

    프로젝트 생성
    1. SnapKit
    2. KingFisher 추가

---
+++ intrmediate/AppStoreApp/AppStoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreApp
+//
+//  Created by 김성준 on 2022/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
