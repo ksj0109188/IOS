6912f2bda0175f590d2afab06ba6e02f880420e9
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-05-12 23:52:12 +0900

    collectionView layout setting
    cell 크기 설정

---
+++ intrmediate/AppStoreApp/AppStoreApp/Scenes/Today/TodayViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TodayViewController.swift
+//  AppStoreApp
+//
+//  Created by 김성준 on 2022/05/12.
+//
+
+import SnapKit
+import UIKit
+
+final class TodayViewController:UIViewController {
+    private lazy var collectionview : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        view.addSubview(collectionview)
+        collectionview.snp.makeConstraints({
+            $0.edges.equalToSuperview()
+        })
+        
+    }
+}
+
+extension TodayViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath)
+        cell.backgroundColor = .white
+        
+        return cell
+    }
+}
+
+extension TodayViewController : UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width - 32.0
+        return CGSize(width: width, height: width)
+    }
+}
