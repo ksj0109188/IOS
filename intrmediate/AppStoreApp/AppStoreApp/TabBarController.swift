1bf8e30894226464bafc8e282855f38160db2b1d
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-05-12 22:46:43 +0900

    UITabBar 구현
    1. 메일
    2. 앱

---
+++ intrmediate/AppStoreApp/AppStoreApp/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  AppStoreApp
+//
+//  Created by 김성준 on 2022/05/10.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    private lazy var todayViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "투데이", image: UIImage(systemName: "mail"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        return viewController
+    }()
+    
+    private lazy var appViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "앱", image: UIImage(systemName: "square.stack.3d.up"), tag: 1)
+        viewController.tabBarItem = tabBarItem
+        return viewController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+        viewControllers = [todayViewController,appViewController]
+    }
+
+
+}
+
