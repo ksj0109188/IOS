6572e0ec2813e0144ec539b0e19ef9cda163f8bf
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-06-09 23:26:08 +0900

    plist 데이터 구조체

---
+++ intrmediate/AppStoreApp/AppStoreApp/RankingFeature.swift
@@ -0,0 +1,15 @@
+//
+//  RankingFeature.swift
+//  AppStoreApp
+//
+//  Created by 김성준 on 2022/06/09.
+//
+
+import Foundation
+
+struct RankingFeature: Decodable{
+    let title : String
+    let description: String
+    let isInPurchaseApp: Bool
+    
+}
---
+++ intrmediate/AppStoreApp/AppStoreApp/Today.swift
@@ -0,0 +1,17 @@
+//
+//  Today.swift
+//  AppStoreApp
+//
+//  Created by 김성준 on 2022/06/09.
+//
+
+import Foundation
+
+struct Today: Decodable {
+    let title: String
+    let subTitle: String
+    let description: String
+    let imageURL: String
+    
+    
+}
