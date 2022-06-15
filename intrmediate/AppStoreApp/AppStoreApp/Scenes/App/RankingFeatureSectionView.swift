cd2f1b82924b444fcb7433cf6f64c2400f70d675
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-06-15 23:28:23 +0900

    shareButton 기능 구현

--- intrmediate/AppStoreApp/AppStoreApp/Scenes/App/RankingFeatureSectionView.swift
+++ intrmediate/AppStoreApp/AppStoreApp/Scenes/App/RankingFeatureSectionView.swift
@@ -9,7 +9,7 @@ import SnapKit
 import UIKit
 
 final class RankingFeatureSectionView: UIView{
-    
+    private var rankingFeatureList: [RankingFeature] = []
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 18.0, weight: .black)
@@ -59,6 +59,8 @@ final class RankingFeatureSectionView: UIView{
         super.init(frame: frame)
         
         setupViews()
+        fetchData()
+        collectionView.reloadData()
     }
     
     required init?(coder:NSCoder){
@@ -75,7 +77,7 @@ extension RankingFeatureSectionView : UICollectionViewDelegateFlowLayout{
 
 extension RankingFeatureSectionView: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        9
+        rankingFeatureList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -83,8 +85,8 @@ extension RankingFeatureSectionView: UICollectionViewDataSource{
             withReuseIdentifier: "RankingFeatureCollectionViewCell" ,
             for: indexPath
         ) as? RankingFeatureCollectionViewCell
-        
-        cell?.setup()
+        let rankingFeautre = rankingFeatureList[indexPath.item]
+        cell?.setup(rankingFeature: rankingFeautre)
         
         return cell ?? UICollectionViewCell()
     }
@@ -119,6 +121,20 @@ private extension RankingFeatureSectionView{
             $0.bottom.equalToSuperview()
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
+        }
+    }
+    
+    func fetchData(){
+        guard let url = Bundle.main.url(forResource: "RankingFeature", withExtension: ".plist") else {
+            return
+        }
+        
+        do{
+            let data = try Data(contentsOf: url)
+            let result = try PropertyListDecoder().decode([RankingFeature].self, from: data)
+            self.rankingFeatureList = result
+        }catch{
+            
         }
     }
 }
--- intrmediate/AppStoreApp/AppStoreApp/Scenes/AppDetailViewController.swift
+++ intrmediate/AppStoreApp/AppStoreApp/Scenes/AppDetailViewController.swift
@@ -48,8 +48,10 @@ final class AppDetailViewController : UIViewController{
             let button = UIButton()
             button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
             button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
             return button
     }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -115,4 +117,11 @@ private extension AppDetailViewController {
             $0.width.equalTo(shareButton.snp.height)
         }
     }
+    
+    @objc func didTapShareButton(){
+        let activityItems:[Any] = [today.title]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+   
 }
