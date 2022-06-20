d78e85788080ee4dec5d17628c158d9ee54acdf9
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-06-20 23:23:12 +0900

    searchViewcontroller
    1. 지하철 검색

--- intrmediate/SubwatStation/SubwatStation/StationSearchViewController.swift
+++ intrmediate/SubwatStation/SubwatStation/StationSearchViewController.swift
@@ -9,33 +9,54 @@ import SnapKit
 import UIKit
 
 class StationSearchViewController: UIViewController {
+    private var numbrOfCells : Int = 0
+    
     private lazy var tableView : UITableView = {
         let tableView = UITableView()
         tableView.dataSource = self
-        
+        tableView.isHidden = true
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems(){
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착 정보"
         
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
         searchController.obscuresBackgroundDuringPresentation = false
-        
+        searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
-        
+     
     }
 
-
+    private func setTableViewLayout(){
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+extension StationSearchViewController : UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numbrOfCells = 10
+        tableView.isHidden = false
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numbrOfCells = 0
+        tableView.isHidden = true
+    }
 }
 
 extension StationSearchViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return numbrOfCells
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
