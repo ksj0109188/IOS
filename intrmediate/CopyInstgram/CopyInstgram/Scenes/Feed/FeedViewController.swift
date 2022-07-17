1c2932b7b8a47ce42b6d737f6942312d43696cc4
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-07-17 16:11:32 +0900

    새 게시물 작성하기

--- intrmediate/CopyInstgram/CopyInstgram/Scenes/Feed/FeedViewController.swift
+++ intrmediate/CopyInstgram/CopyInstgram/Scenes/Feed/FeedViewController.swift
@@ -20,12 +20,40 @@ class FeedViewController: UIViewController {
         return tableView
     }()
     
+    private lazy var imagePickerViewController: UIImagePickerController = {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        return imagePickerController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNaviagationBar()
         setupTableView()
     }
     
+}
+extension FeedViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var selectImage: UIImage?
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            selectImage = editedImage
+        } else if let originImage = info[UIImagePickerController.InfoKey.originalImage] as?
+            UIImage {
+            selectImage = originImage
+        }
+        
+        picker.dismiss(animated: true){ [weak self] in
+            let uploadViewController = UploadViewController(uploadImage: selectImage ?? UIImage())
+            let navigationController = UINavigationController(rootViewController: uploadViewController)
+            navigationController.modalPresentationStyle = .fullScreen
+            
+            self?.present(navigationController, animated: true)
+        }
+    }
 }
 extension FeedViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -50,11 +78,16 @@ private extension FeedViewController {
             image: UIImage(systemName: "plus.app"),
             style: .plain,
             target: self,
-            action: nil
+            action: #selector(didTapUploadButton)
         )
         navigationItem.rightBarButtonItem = uploadButton
         
     }
+    
+    @objc func didTapUploadButton(){
+        present(imagePickerViewController, animated: true)
+    }
+    
     func setupTableView(){
         view.addSubview(tableView)
         tableView.snp.makeConstraints{$0.edges.equalToSuperview()}
