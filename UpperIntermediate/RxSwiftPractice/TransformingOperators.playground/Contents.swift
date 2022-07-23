c6904d2ae167b8b621d86becca63ce548cfff77e
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-07-24 08:43:27 +0900

    rxswift operator

--- UpperIntermediate/RxSwiftPractice/TransformingOperators.playground/Contents.swift
+++ UpperIntermediate/RxSwiftPractice/TransformingOperators.playground/Contents.swift
@@ -82,4 +82,93 @@ let 전국대회 = PublishSubject<선수>()
 부산.점수.onNext(9)
 
 print("-------materialize and dematerialize------------")
+enum 반칙:Error{
+    case 부정출발
+}
+
+struct 달리기선수: 선수 {
+    var 점수: BehaviorSubject<Int>
+}
+
+let 김토끼 = 달리기선수(점수: BehaviorSubject<Int>(value: 0))
+let 박치타 = 달리기선수(점수: BehaviorSubject<Int>(value: 1))
+
+let 달리기100M = BehaviorSubject<선수>(value: 김토끼)
+
+달리기100M
+    .flatMapLatest { 선수 in
+    선수.점수
+        .materialize() //발생한 이벤트를 모두 출력(flatMaLatest 메커니즘과 다르게)
+    }
+    .filter{
+        guard let error = $0.error else {
+            return true
+        }
+        print(error)
+        return false
+    }
+    .dematerialize() //materialize화 된 이벤트를 dematerialize(원래대로)
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+김토끼.점수.onNext(1)
+김토끼.점수.onError(반칙.부정출발)
+김토끼.점수.onNext(3)
+
+달리기100M.onNext(박치타)
+박치타.점수.onNext(4)
+김토끼.점수.onNext(5)
+박치타.점수.onNext(6)
+
+
+print("-------------전화번호 11자리----------------")
+let input = PublishSubject<Int?>()
+
+let list:[Int] = [1]
+
+input
+    .flatMap{
+        $0 == nil
+            ? Observable.empty()
+            : Observable.just($0)
+    }
+    .map{ $0! }
+    .skip(while: {
+        $0 != 0 //만족하면 계속 skip
+    })
+    .take(11){}
+    .toArray()
+    .map{
+        $0.map{ "\($0)" }
+    }
+    .map{ numbers -> String in
+        var numberList = numbers
+        numberList.insert("-", at: 3) //010-
+        numberList.insert("-", at: 8) //010-9188-
+        let number = numberList.reduce(" ", +)
+        return number
+    }
+    .subscribe(onSuccess: {
+        print($0)
+        
+    })
+    .disposed(by: disposeBag)
+    
+input.onNext(10)
+input.onNext(0)
+input.onNext(nil)
+input.onNext(1)
+input.onNext(0)
+input.onNext(4)
+input.onNext(3)
+input.onNext(nil)
+input.onNext(1)
+input.onNext(8)
+input.onNext(9)
+input.onNext(4)
+input.onNext(9)
+input.onNext(4)
+
+
 
