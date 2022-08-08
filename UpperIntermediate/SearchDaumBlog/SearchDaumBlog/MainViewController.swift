08ad4b470f1302a3b3eb7070ad282f2d953fcacd
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-08-09 04:15:04 +0900

    MainController 설정

---
+++ UpperIntermediate/SearchDaumBlog/SearchDaumBlog/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  SearchDaumBlog
+//
+//  Created by 김성준 on 2022/08/08.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    
+}
