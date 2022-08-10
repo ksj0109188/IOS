fde72c61a6f2e7393bbc55daee0024c743717255
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2022-08-11 05:45:28 +0900

    Cell 커스텀
    1. 외부 데이터를 바인드할 Cell 구조체 선언
    2. Cell 디자인 및 바인드된 구조체 데이터 연결

---
+++ UpperIntermediate/SearchDaumBlog/SearchDaumBlog/Entities/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  SearchDaumBlog
+//
+//  Created by 김성준 on 2022/08/10.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailURL : URL?
+    let name : String?
+    let title : String?
+    let dateTime: Date?
+}
